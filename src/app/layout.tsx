9fa05e70f40403944e7c089f7ab0676143c882d8
David Ridwan <ddavidridwan@gmail.com>
2024-09-06 01:52:08 +0700

    feat(user): auth & apply referral

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from "./providers";
 import { fonts } from "./fonts";
 import { cn } from "@/utils/cn";
 import Navbar from "@/components/Navbar";
+import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
   title: "Satsouk App",
@@ -21,6 +22,7 @@ export default function RootLayout({
         fonts.sora.className,
         "bg-zinc-900 text-white flex flex-col items-center"
       )}>
+        <Toaster position="bottom-left" />
         <Providers>
           <Navbar />
           <div className="absolute top-0 left-[50%] w-full h-screen max-w-[1238px] max-h-[708px] translate-x-[-50%] translate-y-[-50%] bg-primary-gradient opacity-25 rounded-[100%] blur-3xl"></div>
