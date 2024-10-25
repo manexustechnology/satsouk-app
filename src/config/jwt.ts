350775724dabc9bef7a976f2a4cb5f5a80f716b6
juniardys <juniardysetio@gmail.com>
2024-10-25 09:35:04 +0700

    ref(env): add some env

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,5 +1,5 @@
 export const JWTConfig = {
   secret: String(process.env.NEXT_PUBLIC_BACKEND_SECRET) || "",
-  key: process.env.NEXT_PUBLIC_BACKEND_KEY || "",
-  signature: process.env.NEXT_PUBLIC_BACKEND_SIGNATURE || "",
+  // key: process.env.NEXT_PUBLIC_BACKEND_KEY || "",
+  // signature: process.env.NEXT_PUBLIC_BACKEND_SIGNATURE || "",
 };
--- src/lib/generate-token.tsx
+++ src/lib/generate-token.tsx
@@ -1,38 +1,38 @@
-import { SignJWT } from "jose";
-import { JWTConfig } from "@/config/jwt";
+// import { SignJWT } from "jose";
+// import { JWTConfig } from "@/config/jwt";
 
-export const generateToken = async (address: string | null, twitterId?: string | null) => {
-  const timestamp = Date.now();
+// export const generateToken = async (address: string | null, twitterId?: string | null) => {
+//   const timestamp = Date.now();
 
-  const secret = new TextEncoder().encode(JWTConfig.secret);
+//   const secret = new TextEncoder().encode(JWTConfig.secret);
 
-  const hashSignature = btoa(
-    `${JWTConfig.key}:${JWTConfig.signature}|i|${timestamp}`
-  );
-  const hashAddress = btoa(
-    `${JWTConfig.key}:${address || ''}|i|${timestamp}`
-  );
+//   const hashSignature = btoa(
+//     `${JWTConfig.key}:${JWTConfig.signature}|i|${timestamp}`
+//   );
+//   const hashAddress = btoa(
+//     `${JWTConfig.key}:${address || ''}|i|${timestamp}`
+//   );
 
-  let hashTwitter = null;
-  if (twitterId) {
-    hashTwitter = btoa(
-      `${JWTConfig.key}:${twitterId || ''}|i|${timestamp}`
-    )
-  }
+//   let hashTwitter = null;
+//   if (twitterId) {
+//     hashTwitter = btoa(
+//       `${JWTConfig.key}:${twitterId || ''}|i|${timestamp}`
+//     )
+//   }
 
-  const alg = "HS256";
+//   const alg = "HS256";
 
-  const token = await new SignJWT({ address })
-    .setProtectedHeader({ alg })
-    .setIssuedAt()
-    .setExpirationTime("2h")
-    .sign(secret);
+//   const token = await new SignJWT({ address })
+//     .setProtectedHeader({ alg })
+//     .setIssuedAt()
+//     .setExpirationTime("2h")
+//     .sign(secret);
 
-  let finalToken = `${token}|i|${hashSignature}|i|${hashAddress}`;
+//   let finalToken = `${token}|i|${hashSignature}|i|${hashAddress}`;
 
-  if (hashTwitter) {
-    finalToken += `|i|${hashTwitter}`;
-  }
+//   if (hashTwitter) {
+//     finalToken += `|i|${hashTwitter}`;
+//   }
 
-  return finalToken;
-};
+//   return finalToken;
+// };
